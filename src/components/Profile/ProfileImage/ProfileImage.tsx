a670176918478f96aeed1efdec076af9c5c14ff9
Mariam Shamsali <mariam.shamsali@gmail.com>
2024-03-20 20:55:07 +0100

    Add border option to profile image and dynamic import for multiple possible extensions profile image file

--- src/components/Profile/ProfileImage/ProfileImage.tsx
+++ src/components/Profile/ProfileImage/ProfileImage.tsx
@@ -1,20 +1,42 @@
 import "./ProfileImage.css";
-import ProfilePicture from "../../../assets/profile.png";
 import clsx from 'clsx';
+import { useEffect, useState } from "react";
 
 type ImageOptions = {
   circular?: boolean;
+  border?: boolean;
 };
 
+
 const ProfileImage = (options: ImageOptions = {}) => {
+  const [ProfilePicture, setProfilePicture] = useState<string | null>(null);
   const classes = clsx(
     'profile__header__image',
-    { 'profile__header__image__circular': options.circular }
+    { 'profile__header__image__circular': options.circular },
+    { 'profile__header__image__border': options.border }
   );
+
+  useEffect(() => {
+    const loadProfilePicture = async () => {
+      const extensions = ['png', 'jpeg', 'jpg', 'gif', 'webp', 'svg'];
+      for (const extension of extensions) {
+        try {
+          const module = await import(`../../../assets/profile.${extension}`);
+          setProfilePicture(module.default);
+          break;
+        } catch (error) {
+          // Ignore error and try next extension
+        }
+      }
+    };
+
+    loadProfilePicture();
+  }, []);
+
 
   return (
     <div className={classes}>
-      <img src={ProfilePicture} alt="Profile" />
+      <img src={ProfilePicture || ''} alt="Profile" />
     </div>
   );
 };
